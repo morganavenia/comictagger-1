0c28572fbc1effd30fe843a5028963fb02b3a049
Timmy Welch <timmy@narnian.us>
2025-03-27 18:56:38 -0700

    Download appimage for the current platform

--- build-tools/get_appimage.py
+++ build-tools/get_appimage.py
@@ -3,14 +3,16 @@ from __future__ import annotations
 import argparse
 import os
 import pathlib
+import platform
 
 try:
     import niquests as requests
 except ImportError:
     import requests
 
+arch = platform.machine()
 parser = argparse.ArgumentParser()
-parser.add_argument("APPIMAGETOOL", default="build/appimagetool-x86_64.AppImage", type=pathlib.Path, nargs="?")
+parser.add_argument("APPIMAGETOOL", default=f"build/appimagetool-{arch}.AppImage", type=pathlib.Path, nargs="?")
 
 opts = parser.parse_args()
 opts.APPIMAGETOOL = opts.APPIMAGETOOL.absolute()
@@ -27,7 +29,8 @@ if opts.APPIMAGETOOL.exists():
     raise SystemExit(0)
 
 urlretrieve(
-    "https://github.com/AppImage/appimagetool/releases/latest/download/appimagetool-x86_64.AppImage", opts.APPIMAGETOOL
+    f"https://github.com/AppImage/appimagetool/releases/latest/download/appimagetool-{arch}.AppImage",
+    opts.APPIMAGETOOL,
 )
 os.chmod(opts.APPIMAGETOOL, 0o0700)
 
